219cb6df8dc841b5bf7f246328b7cc327babfd2c
XYLAS <103405798+XYLAS15@users.noreply.github.com>
2024-03-31 15:39:26 +0530

    Time: 90 ms (53.99%) | Memory: 82.6 MB (53.99%) - LeetSync

--- 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.cpp
+++ 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int minK, int maxK) {
-        long long res = 0;
-        int bad_idx = -1, left_idx = -1, right_idx = -1;
-
-        for (int i = 0; i < nums.size(); ++i) {
-            if (!(minK <= nums[i] && nums[i] <= maxK)) {
-                bad_idx = i;
-            }
-
-            if (nums[i] == minK) {
-                left_idx = i;
-            }
-
-            if (nums[i] == maxK) {
-                right_idx = i;
-            }
-
-            res += max(0, min(left_idx, right_idx) - bad_idx);
+        int minI = -1;
+    int maxI = -1;
+    long ans = 0;
+    int leftBoundary = -1;
+    int n = nums.size();
+    for(int curr = 0; curr < n; curr++) {
+        if(nums[curr] == minK) {
+            minI = curr;
         }
-
-        return res;
+        if(nums[curr] == maxK) {
+            maxI = curr;
+        }
+        // check element is out of bound
+        if(nums[curr] < minK || nums[curr] > maxK) {
+            leftBoundary = curr;
+        }
+        if(minI != -1 && maxI != -1) {
+            int smaller = std::min(minI, maxI);
+            if((smaller - leftBoundary) > 0) {
+                ans = ans + (smaller - leftBoundary);
+            }
+        }
     }
+    return ans;
+}
 };
