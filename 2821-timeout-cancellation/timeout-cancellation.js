da9ba75ebf3c940f951d9fc7e0ecb7dbc38913ab
XYLAS <103405798+XYLAS15@users.noreply.github.com>
2024-10-17 21:34:03 +0530

    Time: 59 ms (84.14%) | Memory: 49.6 MB (48.81%) - LeetSync

---
+++ 2821-timeout-cancellation/timeout-cancellation.js
@@ -0,0 +1,42 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+    // Schedule the function to execute after 't' milliseconds
+    const timeoutId = setTimeout(() => {
+        fn(...args); // Call fn with the provided arguments
+    }, t);
+
+    // Return the cancel function which clears the timeout
+    return function cancelFn() {
+        clearTimeout(timeoutId); // Cancel the execution if called before fn runs
+    };
+};
+
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 5;
+ *  const args = [2], t = 20, cancelTimeMs = 50;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  const maxT = Math.max(t, cancelTimeMs);
+ *           
+ *  setTimeout(cancel, cancelTimeMs);
+ *
+ *  setTimeout(() => {
+ *      console.log(result); // [{"time":20,"returned":10}]
+ *  }, maxT + 15)
+ */
