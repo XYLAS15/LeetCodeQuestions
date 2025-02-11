215a9408549d2ac0a0d0888ef729c82a003c6046
XYLAS <103405798+XYLAS15@users.noreply.github.com>
2025-02-12 00:05:42 +0530

    Time: 1 ms (99.46%) | Memory: 42 MB (88.37%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -1,11 +1,26 @@
 class Solution {
     public String removeOccurrences(String s, String part) {
-        StringBuilder sb = new StringBuilder(s);
-        int index = sb.indexOf(part);
-        while(index != -1) {
-            sb.delete(index, index+part.length());
-            index = sb.indexOf(part);
+        char[] input = s.toCharArray();
+        char[] target = part.toCharArray();
+        char[] resultStack = new char[input.length];
+        int stackSize = 0, targetLength = target.length;
+        char targetEndChar = target[targetLength - 1];
+        for(char currentChar : input) {
+            resultStack[stackSize++] = currentChar;
+
+            if(currentChar == targetEndChar && stackSize >= targetLength) {
+                int i = stackSize - 1, j = targetLength - 1; 
+
+                while(j >= 0 && resultStack[i] == target[j]) {
+                    i--;
+                    j--;
+                }
+
+                if(j < 0) {
+                    stackSize = i + 1;
+                }
+            }
         }
-        return sb.toString();
+        return new String(resultStack , 0, stackSize);
     }
 }
