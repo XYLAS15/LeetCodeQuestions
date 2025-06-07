b21179428b5c5f580d1ad2a7d7f0983c8219c702
XYLAS <103405798+XYLAS15@users.noreply.github.com>
2025-06-07 22:19:59 +0530

    Time: 6 ms (84.51%) | Memory: 43.5 MB (40.65%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,27 +1,44 @@
-class Solution {
-    public String reverseWords(String s) {
-       StringBuilder res = new StringBuilder();
-       int si = s.length()-1; //"the sky is blue"
-    while(si>=0){
-        while(si>=0 && s.charAt(si)==' '){
-            si--;
-        }
-            if(si<0){
-                break;
-            }
-            int ei = si;
-            while(si>=0 && s.charAt(si)!=' '){
+// class Solution {
+//     public String reverseWords(String s) {
+//        StringBuilder res = new StringBuilder();
+//        int si = s.length()-1; //"the sky is blue"
+//     while(si>=0){
+//         while(si>=0 && s.charAt(si)==' '){
+//             si--;
+//         }
+//             if(si<0){
+//                 break;
+//             }
+//             int ei = si;
+//             while(si>=0 && s.charAt(si)!=' '){
 
-                si--;
-            }
-            if(res.length()==0){
-                res.append(s.substring(si+1,ei+1));
-            }
-            else{
-                res.append(" ");
-                res.append(s.substring(si+1,ei+1));
+//                 si--;
+//             }
+//             if(res.length()==0){
+//                 res.append(s.substring(si+1,ei+1));
+//             }
+//             else{
+//                 res.append(" ");
+//                 res.append(s.substring(si+1,ei+1));
+//             }
+//         }
+//       return res.toString();
+// }
+// }
+
+class Solution {
+    public String reverseWords(String str) {
+        str = str.trim(); // Remove leading/trailing spaces
+        String[] words = str.split("\\s+"); // Split by one or more spaces
+        StringBuilder result = new StringBuilder();
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            result.append(words[i]);
+            if (i != 0) {
+                result.append(" ");
             }
         }
-      return res.toString();
-}
+
+        return result.toString();
+    }
 }
