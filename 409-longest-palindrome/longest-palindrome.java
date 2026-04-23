0a008c015d260c42cfdb7331106fae3d5b067d8c
XYLAS <103405798+XYLAS15@users.noreply.github.com>
2026-04-23 23:17:58 +0530

    Time: 1 ms (100.00%) | Memory: 42.9 MB (92.08%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int longestPalindrome(String s) {
+       int set[] = new int[128];
+       int res = 0;
+       int oddCount = 0;
+       for(char ch: s.toCharArray()) {
+        if(set[ch]!=0) {
+            res += 2;
+            set[ch] = 0;
+            oddCount--;
+        } else {
+            set[ch] = 1;
+            oddCount++;
+        }
+       }
+       if(oddCount>0) {
+        res+=1;
+       } 
+       return res;
+    }
+}
