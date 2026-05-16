65f345f14696fef479f848fd9b5d745e546174fd
XYLAS <103405798+XYLAS15@users.noreply.github.com>
2026-05-17 00:14:56 +0530

    Time: 2 ms (99.38%) | Memory: 47.3 MB (23.67%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,25 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-    //     for (int i = 0; i < nums.length; i++) {
-    //         for (int j = i + 1; j < nums.length; j++) {
-    //             if (nums[i] + nums[j] == target) {
-    //                 return new int[] {i, j}; // Correctly return the indices as an array
-    //             }
-    //         }
-    //     }
-    //    throw new IllegalArgumentException("No two sum solution");
-    // }
+        int result[] = new int[2];
+        HashMap<Integer,Integer> map = new HashMap<>();
 
-    Map<Integer,Integer> map = new HashMap<>();
-
-    for(int i=0; i<nums.length; i++) {
-        int complement = target - nums[i];
-
-        if(map.containsKey(complement)) {
-            return new int[] {map.get(complement), i};
+        for(int i=0; i<nums.length; i++) {
+            if(map.containsKey(target - nums[i])){
+                result[1] = i;
+                result[0] = map.get(target - nums[i]);
+                return result;
+            }
+            map.put(nums[i],i);
         }
-        map.put(nums[i],i);
-    }
-         return new int []{};
+        return result;
     }
 }
