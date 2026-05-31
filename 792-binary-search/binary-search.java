ee0d91724209b6a4af2c8d94021c0cbdd73e7fce
XYLAS <103405798+XYLAS15@users.noreply.github.com>
2026-05-31 21:58:16 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (67.45%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -2,16 +2,16 @@ class Solution {
     public int search(int[] nums, int target) {
         int start = 0;
         int end = nums.length-1;
-        while(start <= end){
-            int mid = (start + end)/2;
-            if(nums[mid] == target){
-                return mid;
-            }
-            else if(nums[mid] > target){
-               end = mid - 1; 
-            }
-            else if(nums[mid] < target){
+
+        while(start<=end) {
+            int mid = start + (end -start) / 2;
+            if(nums[mid] == target) return mid;
+
+            else if (target > nums[mid]) {
                 start = mid + 1;
+            }
+            else if (target < nums[mid]) {
+                end = mid - 1;
             }
         }
         return -1;
