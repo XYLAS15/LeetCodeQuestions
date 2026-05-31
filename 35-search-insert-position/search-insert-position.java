16226d79b3ecbb4614a6fbf8c2583c725d551353
XYLAS <103405798+XYLAS15@users.noreply.github.com>
2026-05-31 22:31:35 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (11.75%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,17 +1,20 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length-1;
-        while(start <= end) {
-            int mid = start+(end-start)/2;
-            if(target == nums[mid]){
-                return mid;
-            }
-            else if(nums[mid] > target)
-                end = mid - 1;
-            else
+     int start = 0;
+     int end = nums.length - 1;
+
+     while(start<=end) {
+        int mid = start + (end - start) / 2;
+        if(nums[mid] == target) {
+            return mid;
+        }
+        if(target > nums[mid]) {
             start = mid + 1;
         }
-    return start;
+        if(target < nums[mid]) {
+            end = mid - 1;
+        }
+     }
+     return start;   
     }
 }
